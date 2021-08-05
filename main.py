1efcf4183a0b05ca53ff19e492605665249df2d2
LuizFelipe <luizfelipers19@gmail.com>
2021-08-04 22:41:51 -0300

    implemented the CreateUser function with a POST method to create an User entry in our DB

--- main.py
+++ main.py
@@ -47,7 +47,17 @@ class BlogPost(db.Model):
 
 @app.route("/user", methods=["POST"])
 def create_user():
-    pass
+    #this route will deal with linked lists
+    data = request.get_json()
+    new_user = User(
+        name=data["name"],
+        email=data["email"],
+        address=data["address"],
+        phone=data["phone"],
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message":"User created"}), 200
 
 @app.route("/user/descending_id", methods=["GET"])
 def get_all_users_descending():
