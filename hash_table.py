2239599a3e472dd2096c1d5e461693ee789209f1
LuizFelipe <luizfelipers19@gmail.com>
2021-08-07 11:57:00 -0300

    created a HashTable.py file to implement the HashTable (data structure similar to a dictionary)

---
+++ hash_table.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, data=None, next_node=None):
+        self.data = data,
+        self.next_node = next_node
+
+class Data:
+    def __init__(self,key, value):
+        self.key = key,
+        self.value = value
+
+class Hashtable:
+    def __init__(self, table_size):
+        self.table_size = table_size
