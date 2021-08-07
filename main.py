65c640ef072ac105ae326611658fe382c48c24b3
LuizFelipe <luizfelipers19@gmail.com>
2021-08-06 22:47:46 -0300

    fixed the Insert_at_end function in the LinkedList class, and implemented the ascending_order of the GET method to retrieve users by id

--- main.py
+++ main.py
@@ -80,7 +80,21 @@ def get_all_users_descending():
 
 @app.route("/user/ascending_id", methods=["GET"])
 def get_all_users_ascending():
-    pass
+    users = User.query.all()
+    all_users_ll = linked_list.LinkedList()
+
+    for user in users:
+        all_users_ll.insert_at_end(
+            {
+                "id": user.id,
+                "name": user.id,
+                "email": user.email,
+                "address": user.address,
+                "phone": user.phone,
+            }
+        )
+
+    return jsonify(all_users_ll.to_list()), 200
 
 @app.route("/user/<user_id>", methods=["GET"])
 def get_user_by_id(user_id):
