8ec5a0dfe25fb73e7177ea6a0f337339a503e19a
LuizFelipe <luizfelipers19@gmail.com>
2021-08-07 13:08:51 -0300

    implemented the hashing function and a method to add key:values in our hash_table

--- hash_table.py
+++ hash_table.py
@@ -11,3 +11,17 @@ class Data:
 class Hashtable:
     def __init__(self, table_size):
         self.table_size = table_size
+        self.hash_table = [None] * table_size
+
+    def custom_hash(self,key):
+        hash_value = 0
+        for i in key:
+            hash_value += ord(i)
+            hash_value = (hash_value * ord(i)) % self.table_size
+        return hash_value
+
+    def add_key_value(self, key, value):
+        hashed_key = self.custom_hash(key)
+        if self.hash_table[hashed_key] is None:
+            self.hash_table[hashed_key] = Node(Data(key, value), None)
+        else:
