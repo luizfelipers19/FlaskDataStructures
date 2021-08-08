d7b32d971774edeae9f95cb611a3c8783487ade8
LuizFelipe <luizfelipers19@gmail.com>
2021-08-08 20:56:21 -0300

    starting to implement the BinarySearchTree structure

---
+++ binary_search_tree.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
