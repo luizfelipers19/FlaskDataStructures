95f25b1b831b7a16b274760c9ce276c77359ebe1
LuizFelipe <luizfelipers19@gmail.com>
2021-08-09 22:00:12 -0300

    implemented our BST search method in a recursive way

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -29,6 +29,24 @@ class BinarySearchTree:
         else:
             self._insert_recursive(data, self.root)
 
+    def _search_recursive(self, blog_post_id, node):
+        if node.left == None and node.right == None:
+            return False
+
+        if blog_post_id == node.data["id"]:
+            return  node.data
+
+        if blog_post_id < node.data["id"]:
+            if blog_post_id == node.left.data["id"]:
+                return node.left.data
+            return self._search_recursive(blog_post_id, node.left)
+
+        if blog_post_id > node.data["id"]:
+            if blog_post_id == node.right.data["id"]:
+                return node.right.data
+            return self._search_recursive(blog_post_id, node.right)
+
+
     def search(self, blog_post_id):
         blog_post_id = int(blog_post_id)
         if self.root is None:
